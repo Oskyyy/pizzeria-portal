84ae5d8ceda908943b5c878952bac9c613c2537e
Oskyyy <oskarwro@gmail.com>
2020-01-27 20:03:43 +0100

    Material UI

--- src/components/views/WaiterOrderEdit/WaiterOrderEdit.js
+++ src/components/views/WaiterOrderEdit/WaiterOrderEdit.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from './TablesEventsNew.module.scss';
+import styles from './WaiterOrderEdit.module.scss';
 import Subtitle from '../../common/Subtitle/Subtitle';
 import Title from '../../common/Title/Title';
 import Paper from '@material-ui/core/Paper';
@@ -10,34 +10,29 @@ import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import DPicker from '../../features/DateTimePicker/DatePicker';
-import TPicker from '../../features/DateTimePicker/TimePicker';
-import BasicTextField from '../../features/Form/TextField';
-import BasicNumberField from '../../features/Form/NumberField';
 
-function createData(info, details) {
-  return { info, details };
+function createData(details, products) {
+  return { details, products };
 }
 
 const rows = [
-  createData('Date:', <DPicker />),
-  createData('Name:', <BasicTextField />),
-  createData('Ship To:', <BasicTextField />),
-  createData('Table No.:', <BasicNumberField />),
-  createData('Hour:', <TPicker />),
-  createData('People:', <BasicNumberField />),
+  createData('Table No.', 1),
+  createData('Menu Products', 'pizza tonno, cola, ice-cream, chips'),
+  createData('Options', 'additional cheese, black olives, no onion'),
+  createData('Subtotal', 305, 3.7, 67, 4.3),
+  createData('Total Price', 356, 16.0, 49, 3.9),
 ];
 
-const TablesEventNew = () => (
+const WaiterOrderEdit = () => (
   <div className={styles.component}>
     <CssBaseline />
-    <Title>Tables New Event</Title>
+    <Title>Waiter Edit Order</Title>
     <Paper className={styles.component}>
       <Table className={styles.table} size="small">
         <TableHead>
           <TableRow>
             <TableCell align="left">
-              <Subtitle>Event no. ___</Subtitle>
+              <Subtitle>Order no. 320</Subtitle>
             </TableCell>
             <TableCell />
             <TableCell />
@@ -45,10 +40,10 @@ const TablesEventNew = () => (
         </TableHead>
         <TableBody>
           {rows.map(row => (
-            <TableRow key={row.details}>
-              <TableCell align="left">{row.info}</TableCell>
+            <TableRow key={row.products}>
+              <TableCell align="left">{row.details}</TableCell>
               <TableCell component="th" scope="row">
-                {row.details}
+                {row.products}
               </TableCell>
               <TableCell align="right">
                 <Button
@@ -56,7 +51,7 @@ const TablesEventNew = () => (
                   color="primary"
                   variant="outlined"
                 >
-                  Add
+                  Edit
                 </Button>
               </TableCell>
             </TableRow>
@@ -67,4 +62,4 @@ const TablesEventNew = () => (
   </div>
 );
 
-export default TablesEventNew;
+export default WaiterOrderEdit;
--- src/components/views/WaiterOrderNew/WaiterOrderNew.js
+++ src/components/views/WaiterOrderNew/WaiterOrderNew.js
@@ -1,9 +1,64 @@
 import React from 'react';
-import styles from './WaiterOrderNew.scss';
+import styles from './WaiterOrderNew.module.scss';
+import Subtitle from '../../common/Subtitle/Subtitle';
+import Title from '../../common/Title/Title';
+import Paper from '@material-ui/core/Paper';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+
+function createData(details, products) {
+  return { details, products };
+}
+
+const rows = [
+  createData('Table No.', null),
+  createData('Menu Products', null),
+  createData('Options', null),
+  createData('Subtotal', null),
+  createData('Total Price', null),
+];
 
 const WaiterOrderNew = () => (
   <div className={styles.component}>
-    <h2>WaiterOrderNew view</h2>
+    <CssBaseline />
+    <Title>Waiter New Order</Title>
+    <Paper className={styles.component}>
+      <Table className={styles.table} size="small">
+        <TableHead>
+          <TableRow>
+            <TableCell align="left">
+              <Subtitle>Order no. ___</Subtitle>
+            </TableCell>
+            <TableCell />
+            <TableCell />
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map(row => (
+            <TableRow key={row.products}>
+              <TableCell align="left">{row.details}</TableCell>
+              <TableCell component="th" scope="row">
+                {row.products}
+              </TableCell>
+              <TableCell align="right">
+                <Button
+                  className={styles.button}
+                  color="primary"
+                  variant="outlined"
+                >
+                  Add
+                </Button>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </Paper>
   </div>
 );
 
